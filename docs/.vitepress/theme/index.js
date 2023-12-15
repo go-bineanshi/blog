4314795f62753886d189f9be32bc3adb37e818fc
bineanshi <bineanshi@163.com>
2023-12-15 15:31:14 +0800

    添加图片显示模块,代码展开模块

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -6,18 +6,33 @@ import MyLayout from './MyLayout.vue'
 import vitepressBackToTop from 'vitepress-plugin-back-to-top'
 import 'vitepress-plugin-back-to-top/dist/style.css'
 
-
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useData, useRoute } from 'vitepress';
+import codeblocksFold from 'vitepress-plugin-codeblocks-fold'; // import method
+import 'vitepress-plugin-codeblocks-fold/style/index.scss'; // import style
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: MyLayout,
 
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp(ctx) {
     vitepressBackToTop({
       // default
       threshold: 300,
       duration: 500,
     })
+  },
+  setup() {
+    // Get route
+    const route = useRoute();
+    // Using
+    imageViewer(route);
+
+    const { frontmatter } = useData();
+    // basic use
+    codeblocksFold({ route, frontmatter }, true, 300);
   }
 }
--- makefile
+++ makefile
@@ -1,12 +1,14 @@
 push:
-	@ssh ${USER} 'rm -rf /home/ecs-user/blog'
+	@ssh ${USER} 'rm -rf /home/ecs-user/blog/*'
 	@scp -r docs/.vitepress/dist/ ${USER}:/home/ecs-user/blog
+	@rm -rf docs/.vitepress/dist/
 	@scp docker-compose.yaml ${USER}:/home/ecs-user/docker-compose.blog.yaml
 build:
 	@yarn docs:build
-	@scp -r docs/.vitepress/dist/ ${USER}:/home/ecs-user/blog
-	@rm -rf docs/.vitepress/dist/
 start:
 	@ssh ${USER} 'docker stack deploy -c docker-compose.blog.yaml blog'
 stop:
 	@ssh ${USER} 'docker stack rm blog'
+restart:
+	@make stop
+	@make start
